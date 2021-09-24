46334541d723999c1210974384fc4690723901ad
Rith0107 <90136325+Rith0107@users.noreply.github.com>
2021-09-24 23:15:13 +0530

    Update mysqlhandler.py

--- lab_3/dbhandler/mysqlhandler.py
+++ lab_3/dbhandler/mysqlhandler.py
@@ -1,68 +1,65 @@
 import mysql.connector
 
-class mysqlhandler:
-    def __init__(self):
-        pass;
-
-    def myfunc(self):
-
+class sqlhandler:
+    
+    def myfunc(data,f):
+        
         from mysql.connector import Error
+
+
         try:
-            pwd = input("Enter mysql password: ")
             connection = mysql.connector.connect(host='localhost',
                                                  database='UserDB',
                                                  user='root',
-                                                 password= pwd)
+                                                 password='rithwik17')
             if connection.is_connected():
                 db_Info = connection.get_server_info()
                 cursor = connection.cursor(buffered=True)
                 cursor.execute("select database();")
-                
-                
-                def display_users() :
-                    query= ("Select * from user_data")
-                    cursor.execute(query)
-                    for i in cursor:
-                        print(i)
 
-                def add_user() :
-                    na=input("Enter name of user : ")
-                    ph=input("Enter phone number of user : ")
-                    do=input("Enter dob of user:")
+
+                def display_table(data) :
+                    query= ("select * from user_data")
+                    cursor.execute(query)
+                    for rows in cursor:
+                        print(rows)
+
+                def add_user(data) :
+                    na=data["name"]
+                    ph=data["phno"]
+                    do=data["dob"]
                     query=("insert into user_data(name,phno,dob)" "values(%s,%s,%s)");
                     data=(na,ph,do)
                     cursor.execute(query,data)
                     connection.commit()
 
-                def delete_user() :
-                    na=input("Enter name of the user to delete : ")
+                def delete_user(data) :
+                    na=data["name"]
                     query=("delete from user_data where name='%s' "%(na));
                     cursor.execute(query)
                     connection.commit()
 
-                def update_user() :
-                    na=input("Enter the user's name whose details you want to update :")
-                    field=input("Enter the field you want to update :")
-                    value=input("The value you want to update the field with :")
+                def update_user(data) :
+                    na=data["name"]
+                    field=data["field"]
+                    value=data["value"]
                     query=("update user_data set %s='%s' where name='%s' " %(field,value,na))
                     cursor.execute(query)
                     connection.commit()
+
                 
-                f=int(input("Function Codes :\n Enter 1 for displaying users \n Enter 2 for adding user data \n Enter 3 for deleting a user \n Enter 4 for updating a user data \n"))
                 if f==1 :
-                    display_users()
+                    display_table(data)
                 elif f==2 :
-                    add_user()
+                    add_user(data)
                 elif f==3 :
-                    delete_user()
+                    update_users(data)
                 elif f==4 :
-                    update_user()
+                    delete_user(data)
                 else :
-                    print("give a valid input") 
+                    print("Wrong Format") 
 
         except Error as e:
             print("Error", e)
-                
-if __name__ == "__main__":
-    mydb = mysqlhandler()
-    mydb.myfunc()
+            
+    
